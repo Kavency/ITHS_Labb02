c73cc050ddf3ef8c7181830d7bb52e80c9af2ffc
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-25 20:51:47 +0200

    Update GetName.

---
+++ DungeonCrawler/GameLogic/NameProvider.cs
@@ -0,0 +1,24 @@
+﻿
+namespace DungeonCrawler.GameLogic
+{
+    static internal class NameProvider
+    {
+        static string[] names = {
+                "Aric", "Thalion", "Eryndor", "Kael", "Draven", 
+                "Fenris", "Galen", "Roderic", "Zephyr", "Lucian",
+                "Dorian", "Alaric", "Balthazar", "Cedric", "Eldrin", 
+                "Faelan", "Garrick", "Haldor", "Ivor", "Jareth",
+                "Kieran", "Leoric", "Maelor", "Niall", "Orin",
+                "Elara", "Seraphina", "Lyra", "Arwen", "Isolde",
+                "Thalia", "Nymeria", "Elysia", "Calista", "Freya",
+                "Gwendolyn", "Aeliana", "Brienne", "Cerys", "Daenerys",
+                "Elowen", "Fionna", "Gwyneth", "Hespera", "Ilyana",
+                "Jessamine", "Kaida", "Liora", "Morgana", "Nerys" };
+
+        static public string GetName()
+        {
+            Random rnd = new();
+            return names[rnd.Next(1, names.Length)];
+        }
+    }
+}
