ed6f730f3bb63c0795dd826b2b6aa2ae7b9f7548
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-25 19:00:27 +0200

    Add LevelElement class.

---
+++ DungeonCrawler/Elements/LevelElement.cs
@@ -0,0 +1,9 @@
+﻿abstract internal class LevelElement
+{
+    public int xPosition { get; set; }
+    public int yPosition { get; set; }
+    public char mapSymbol { get; set; }
+    public ConsoleColor symbolColour { get; set; }
+
+    public void Draw() { }
+}
