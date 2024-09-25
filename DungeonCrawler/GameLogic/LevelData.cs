54f65b051d981b16efe6bf3ffad269d0fb1ab9e6
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-25 21:42:47 +0200

    Update LevelData.

--- DungeonCrawler/GameLogic/LevelData.cs
+++ DungeonCrawler/GameLogic/LevelData.cs
@@ -1,6 +1,23 @@
-﻿namespace DungeonCrawler.GameLogic
+﻿using DungeonCrawler.Elements;
+
+namespace DungeonCrawler.GameLogic
 {
-    internal class LevelData
+    static internal class LevelData
     {
+        static private List<LevelElement> elements = new();
+
+        static public void Load(string fileName)
+        {
+            // TODO: Refactor?
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                int character;
+
+                while ((character = reader.Read()) != -1)
+                {
+                    Console.Write((char)character);
+                }
+            }
+        }
     }
 }
--- DungeonCrawler/Program.cs
+++ DungeonCrawler/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace DungeonCrawler
+﻿using DungeonCrawler.GameLogic;
+
+namespace DungeonCrawler
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // TODO: Make path relate....
+            LevelData.Load("C:\\Users\\Kavency\\source\\repos\\Kavency\\ITHS_Labb02\\DungeonCrawler\\Levels\\Level1.txt");
+            Console.ReadLine();
         }
     }
 }
