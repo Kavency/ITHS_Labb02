f6db24113f7d640344c177424312f932ff20364e
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-25 20:55:20 +0200

    Update Rat.

--- DungeonCrawler/Elements/Rat.cs
+++ DungeonCrawler/Elements/Rat.cs
@@ -1,7 +1,14 @@
-﻿namespace DungeonCrawler.Elements
+﻿using DungeonCrawler.GameLogic;
+
+namespace DungeonCrawler.Elements
 {
     internal class Rat : Enemy
     {
+        public Rat()
+        {
+            Name = NameProvider.GetName();
+            Health = 100;
+        }
         public override void Update()
         {
             throw new NotImplementedException();
