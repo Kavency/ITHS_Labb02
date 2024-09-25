8a49174c13c28a507ffb765144ff361f988578a2
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-25 20:56:27 +0200

    Update snake.

--- DungeonCrawler/Elements/Snake.cs
+++ DungeonCrawler/Elements/Snake.cs
@@ -1,7 +1,14 @@
-﻿namespace DungeonCrawler.Elements
+﻿using DungeonCrawler.GameLogic;
+
+namespace DungeonCrawler.Elements
 {
     internal class Snake : Enemy
     {
+        public Snake()
+        {
+            Name = NameProvider.GetName();
+            Health = 100;
+        }
         public override void Update()
         {
             throw new NotImplementedException();
