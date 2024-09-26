1668db1884a24cd5f96d0d0bcce4f2ef9b0011fd
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-26 19:35:35 +0200

    Update

--- DungeonCrawler/Elements/Player.cs
+++ DungeonCrawler/Elements/Player.cs
@@ -1,7 +1,17 @@
-﻿namespace DungeonCrawler.Elements
+﻿using DungeonCrawler.GameLogic;
+
+namespace DungeonCrawler.Elements
 {
     internal class Player : LevelElement
     {
+        public string Name { get; set; }
+        public int Health { get; set; }
 
+        public Player()
+        {
+            Name = NameProvider.GetName();
+            Health = 100;
+            SymbolColour = ConsoleColor.Yellow;
+        }
     }
 }
