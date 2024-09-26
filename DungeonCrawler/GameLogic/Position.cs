2e2592651fa892f01be142c27a759bd0fa68a148
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-26 11:17:18 +0200

    Add Position struct.

---
+++ DungeonCrawler/GameLogic/Position.cs
@@ -0,0 +1,13 @@
+﻿namespace DungeonCrawler.GameLogic
+{
+    internal struct Position
+    {
+        public int xPosition { get; set; }
+        public int yPosition { get; set; }
+
+        public Position()
+        {
+            (xPosition, yPosition) = Console.GetCursorPosition();
+        }
+    }
+}
