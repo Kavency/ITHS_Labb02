6b2d9d9817e05190c677b9ff88ff956eb966d8ef
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-26 11:17:57 +0200

    Add AddElementToListOfElements method.

--- DungeonCrawler/GameLogic/LevelData.cs
+++ DungeonCrawler/GameLogic/LevelData.cs
@@ -11,13 +11,44 @@ namespace DungeonCrawler.GameLogic
             // TODO: Refactor?
             using (StreamReader reader = new StreamReader(fileName))
             {
-                int character;
+                int characterAsInt;
 
-                while ((character = reader.Read()) != -1)
+                // TODO: Clean up this mess!
+                while ((characterAsInt = reader.Read()) != -1)
                 {
-                    Console.Write((char)character);
+                    Console.Write((char)characterAsInt);
+                    
+                    if (characterAsInt != 32)
+                    {
+                        AddElementToListOfElements(characterAsInt);
+                    }
                 }
             }
+        }
+
+        static public void AddElementToListOfElements(int characterAsInt)
+        {
+            if (characterAsInt == 35) // Wall
+            {
+                Wall wall = new() { mapSymbol = (char)characterAsInt };
+                elements.Add(wall);
+            }
+            else if (characterAsInt == 114) // Rat
+            {
+                Rat rat = new() { mapSymbol = (char)characterAsInt };
+
+                elements.Add(rat);
+            }
+            else if (characterAsInt == 115) // Snake
+            {
+                Snake snake = new() { mapSymbol = (char)characterAsInt };
+                elements.Add(snake);
+            }
+            //else if (character == 64) // Player
+            //{
+            //    Wall wall = new() { mapSymbol = (char)character };
+            //    elements.Add(wall);
+            //}
         }
     }
 }
