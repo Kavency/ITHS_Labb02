16d46af78066d4fcd892d3d2053609ff7e081ca2
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-27 17:08:54 +0200

    Add Dice prop.

--- DungeonCrawler/Elements/Enemy.cs
+++ DungeonCrawler/Elements/Enemy.cs
@@ -1,9 +1,13 @@
-﻿namespace DungeonCrawler.Elements
+﻿using DungeonCrawler.GameLogic;
+
+namespace DungeonCrawler.Elements
 {
     abstract internal class Enemy : LevelElement
     {
         public string Name { get; set; }
         public float Health { get; set; }
+        public Dice AttackDice { get; set; }
+        public Dice DefenceDice { get; set; }
 
         abstract public void Update();
     }
