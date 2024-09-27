853fad600c3ee24764a911f2f71206e921bfd4a3
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-27 06:54:36 +0200

    Add methods PlatGame and SetupGame.

--- DungeonCrawler/GameLogic/Game.cs
+++ DungeonCrawler/GameLogic/Game.cs
@@ -1,6 +1,24 @@
-﻿namespace DungeonCrawler.GameLogic
+﻿using DungeonCrawler.Elements;
+
+namespace DungeonCrawler.GameLogic
 {
     internal class Game
     {
+        Player player;
+        public void SetupGame()
+        {
+            Console.Title = "Dungeon Crawler Deluxe Edition";
+            string filePath = @"Levels\TestLevel.txt";
+            LevelData.Load(filePath);
+        }
+        public void PlayGame()
+        {
+            while (true)
+            {
+                // Game Loop
+            }
+        }
+
+
     }
 }
