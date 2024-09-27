6c6594b0518c3fda277f1e31649db01df8924945
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-27 20:09:41 +0200

    Add player collision.

--- DungeonCrawler/Elements/Player.cs
+++ DungeonCrawler/Elements/Player.cs
@@ -1,6 +1,4 @@
 ﻿using DungeonCrawler.GameLogic;
-using System.Xml.Linq;
-using System;
 
 namespace DungeonCrawler.Elements
 {
@@ -9,6 +7,7 @@ namespace DungeonCrawler.Elements
         public string Name { get; set; }
         public int Health { get; set; }
 
+        enum Directions { North, South, West, East }
         ConsoleKeyInfo keyInfo;
         ConsoleKey keyPressed;
 
@@ -23,29 +22,86 @@ namespace DungeonCrawler.Elements
         {
             Console.SetCursorPosition(XPosition, YPosition);
             Console.Write(" ");
+            LevelElement collisionObject = null;
 
             if (Console.KeyAvailable)
             {
                 keyInfo = Console.ReadKey(true);
                 keyPressed = keyInfo.Key;
+
+                
                 switch (keyPressed)
                 {
                     // TODO: Check for other objects.
                     case ConsoleKey.W:
-                        this.YPosition--;
+                        if (CheckForCollision(Directions.North))
+                            break;
+                        else
+                            this.YPosition--;
                         break;
                     case ConsoleKey.S:
-                        this.YPosition++;
+                        if (CheckForCollision(Directions.South))
+                            break;
+                        else
+                            this.YPosition++;
                         break;
                     case ConsoleKey.A:
-                        this.XPosition--;
+                        if (CheckForCollision(Directions.West))
+                            break;
+                        else
+                            this.XPosition--;
                         break;
                     case ConsoleKey.D:
-                        this.XPosition++;
+                        if (CheckForCollision(Directions.East))
+                            break;
+                        else
+                            this.XPosition++;
                         break;
                     default:
                         break;
                 }
+            }
+            bool CheckForCollision(Enum direction)
+            {
+                LevelElement collisionObject;
+
+                if(Directions.North.Equals(direction))
+                {
+                    var item = LevelData.MapElements.Find(item => item.XPosition == this.XPosition && item.YPosition == this.YPosition - 1);
+                    if(item != null)
+                        return true;
+                    else
+                        return false;
+                }
+                if (Directions.South.Equals(direction))
+                {
+                    var item = LevelData.MapElements.Find(item => item.XPosition == this.XPosition && item.YPosition == this.YPosition + 1 );
+                    if (item != null)
+                            return true;
+                    else
+                        return false;
+
+                }
+                if (Directions.West.Equals(direction))
+                {
+                    var item = LevelData.MapElements.Find(item => item.YPosition == this.YPosition && item.XPosition == this.XPosition - 1);
+                    if (item != null)
+                        return true;
+                    else
+                        return false;
+
+                }
+                if (Directions.East.Equals(direction))
+                {
+                    var item = LevelData.MapElements.Find(item => item.YPosition == this.YPosition && item.XPosition == this.XPosition + 1);
+                    if (item != null)
+                        return true;
+                    else
+                        return false;
+
+                }
+                else
+                    return false;
             }
         }
     }
