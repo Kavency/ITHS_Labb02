0911eaea863db88a49af7e7dfcf62e71becd4cfe
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-27 17:09:51 +0200

    Change Sleep().

--- DungeonCrawler/GameLogic/Game.cs
+++ DungeonCrawler/GameLogic/Game.cs
@@ -24,7 +24,7 @@ namespace DungeonCrawler.GameLogic
             {
                 player.PlayerMovement();
                 player.Draw();
-                Thread.Sleep(250);
+                Thread.Sleep(50);
                 // Game Loop
             }
         }
