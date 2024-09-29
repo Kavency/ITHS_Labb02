df18f23efd3f9599c8ab410e7446eec28489122e
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-29 21:56:58 +0200

    Refactor.

--- DungeonCrawler/GameLogic/DistanceController.cs
+++ DungeonCrawler/GameLogic/DistanceController.cs
@@ -6,19 +6,17 @@ namespace DungeonCrawler.GameLogic
     {
         public static void CheckDistance(LevelElement player, LevelElement element)
         {
-            // d = √[(x2 − x1)^2 + (y2 − y1)^2]
+            // Euclides Formula for distance  d = √[(x2 − x1)^2 + (y2 − y1)^2]
             int distance = 0;
-            distance = (int)Math.Sqrt(Math.Pow((element.XPosition - player.XPosition), 2) + Math.Pow((element.YPosition - player.YPosition), 2));
+            distance = (int)Math.Sqrt(Math.Pow((element.XPosition - player.XPosition), 2) 
+                + Math.Pow((element.YPosition - player.YPosition), 2));
 
-            if (distance < 6)
+            if (distance < 5)
             {
                 element.IsVisible = true;
 
                 if (element is Wall wall)
                     wall.HasBeenDetected = true;
-
-                Console.SetCursorPosition(1, 22);
-                Console.WriteLine($"Distance to {element} is {distance}");
             }
             else
             {
