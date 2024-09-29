993c9c3a4dcb5d52f5bbb7be9d7d42e8e845a1c8
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-29 18:25:53 +0200

    Refactoring.

--- DungeonCrawler/Elements/Player.cs
+++ DungeonCrawler/Elements/Player.cs
@@ -6,7 +6,7 @@ namespace DungeonCrawler.Elements
     {
         public string Name { get; set; }
         public int Health { get; set; }
-
+        
         ConsoleKeyInfo keyInfo;
         ConsoleKey keyPressed;
         
@@ -15,59 +15,86 @@ namespace DungeonCrawler.Elements
             Name = NameProvider.GetName();
             Health = 100;
             SymbolColour = ConsoleColor.Yellow;
+            MapSymbol = '@';
         }
 
         public void PlayerMovement()
         {
-            Console.SetCursorPosition(XPosition, YPosition);
-            Console.Write(" ");
-
-            if (Console.KeyAvailable)
+            bool isKeyPressed = false;
+            
+            while(!isKeyPressed)
             {
-                keyInfo = Console.ReadKey(true);
-                keyPressed = keyInfo.Key;
-
-                switch (keyPressed)
+                if (Console.KeyAvailable)
                 {
-                    case ConsoleKey.W:
-                        if (CollisionHandler.CheckForCollision(Directions.North, this))
-                        {
-                            CollisionHandler.Collide();
-                            break;
-                        }
-                        else
-                            this.YPosition--;
-                        break;
-                    case ConsoleKey.S:
-                        if (CollisionHandler.CheckForCollision(Directions.South, this))
-                        {
-                            CollisionHandler.Collide();
-                            break;
-                        }
-                        else
-                            this.YPosition++;
-                        break;
-                    case ConsoleKey.A:
-                        if (CollisionHandler.CheckForCollision(Directions.West, this))
-                        {
-                            CollisionHandler.Collide();
-                            break;
-                        }
-                        else
-                            this.XPosition--;
-                        break;
-                    case ConsoleKey.D:
-                        if (CollisionHandler.CheckForCollision(Directions.East, this))
-                        {
-                            CollisionHandler.Collide();
-                            break;
-                        }
-                        else
-                            this.XPosition++;
-                        break;
-                    default:
-                        break;
+                    keyInfo = Console.ReadKey(true);
+                    keyPressed = keyInfo.Key;
+
+                    switch (keyPressed)
+                    {
+                        case ConsoleKey.W:
+                            if (CollisionHandler.CheckForCollision(Directions.North, this))
+                            {
+                                CollisionHandler.Collide();
+                                isKeyPressed = true;
+                                break;
+                            }
+                            else
+                            {
+                                ClearPreviousPosition();
+                                this.YPosition--;
+                                isKeyPressed = true;
+                                break;
+                            }
+                        case ConsoleKey.S:
+                            if (CollisionHandler.CheckForCollision(Directions.South, this))
+                            {
+                                CollisionHandler.Collide();
+                                isKeyPressed = true;
+                                break;
+                            }
+                            else
+                            {
+                                ClearPreviousPosition();
+                                this.YPosition++;
+                                isKeyPressed = true;
+                                break;
+                            }
+                        case ConsoleKey.A:
+                            if (CollisionHandler.CheckForCollision(Directions.West, this))
+                            {
+                                CollisionHandler.Collide();
+                                isKeyPressed = true;
+                                break;
+                            }
+                            else
+                            {
+                                ClearPreviousPosition();
+                                this.XPosition--;
+                                isKeyPressed = true;
+                                break;
+                            }
+                        case ConsoleKey.D:
+                            if (CollisionHandler.CheckForCollision(Directions.East, this))
+                            {
+                                CollisionHandler.Collide();
+                                isKeyPressed = true;
+                                break;
+                            }
+                            else
+                            {
+                                ClearPreviousPosition();
+                                this.XPosition++;
+                                isKeyPressed = true;
+                                break;
+                            }
+                    }
                 }
+            }
+
+            void ClearPreviousPosition()
+            {
+                Console.SetCursorPosition(XPosition, YPosition);
+                Console.Write(" ");
             }
         }
     }
