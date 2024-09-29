81e35fe7f14e06df482aa2276f14931ca3466ffd
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-29 11:46:15 +0200

    Add CollisionHandler.

---
+++ DungeonCrawler/GameLogic/CollisionHandler.cs
@@ -0,0 +1,68 @@
+﻿using DungeonCrawler.Elements;
+
+namespace DungeonCrawler.GameLogic
+{
+    internal static class CollisionHandler
+    {
+        static LevelElement collisionObject;
+
+        public static bool CheckForCollision(Enum directionMoved, LevelElement character)
+        {
+            if (Directions.North.Equals(directionMoved))
+            {
+                var item = LevelData.MapElements.Find(item => item.XPosition == character.XPosition && item.YPosition == character.YPosition - 1);
+
+                if (item != null)
+                {
+                    collisionObject = item;
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else if (Directions.South.Equals(directionMoved))
+            {
+                var item = LevelData.MapElements.Find(item => item.XPosition == character.XPosition && item.YPosition == character.YPosition + 1);
+
+                if (item != null)
+                {
+                    collisionObject = item;
+                    return true;
+                }
+                else
+                    return false;
+
+            }
+            else if (Directions.West.Equals(directionMoved))
+            {
+                var item = LevelData.MapElements.Find(item => item.YPosition == character.YPosition && item.XPosition == character.XPosition - 1);
+
+                if (item != null)
+                {
+                    collisionObject = item;
+                    return true;
+                }
+                else
+                    return false;
+
+            }
+            else
+            {
+                var item = LevelData.MapElements.Find(item => item.YPosition == character.YPosition && item.XPosition == character.XPosition + 1);
+
+                if (item != null)
+                {
+                    collisionObject = item;
+                    return true;
+                }
+                else
+                    return false;
+            }
+        }
+        public static void Collide()
+        {
+            Console.SetCursorPosition(1, 22);
+            Console.WriteLine(collisionObject);
+        }
+    }
+}
