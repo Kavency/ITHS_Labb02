d6cef325af9db1adb372915d21986305098cdf71
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-29 20:09:29 +0200

    Add ClearOldPosition method.

--- DungeonCrawler/GameLogic/CollisionHandler.cs
+++ DungeonCrawler/GameLogic/CollisionHandler.cs
@@ -62,7 +62,13 @@ namespace DungeonCrawler.GameLogic
         public static void Collide()
         {
             Console.SetCursorPosition(1, 22);
-            Console.WriteLine(collisionObject);
+            Console.WriteLine(collisionObject + "             ");
+        }
+
+        public static void ClearOldPosition(LevelElement element)
+        {
+            Console.SetCursorPosition(element.XPosition, element.YPosition);
+            Console.Write(" ");
         }
     }
 }
