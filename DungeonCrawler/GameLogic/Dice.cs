ed19985aedf3e10205a60edeff9b0180da4cd7c3
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-30 18:38:23 +0200

    Add ThrowDice method.

--- DungeonCrawler/GameLogic/Dice.cs
+++ DungeonCrawler/GameLogic/Dice.cs
@@ -2,10 +2,32 @@
 {
     internal class Dice
     {
+        public int NoOfDice { get; set; }
+        public int NoOfSides { get; set; }
+        public int Modifier { get; set; }
+        public Dice(int numberOfDice, int numberOfSides, int modifier)
+        {
+            NoOfDice = numberOfDice;
+            NoOfSides = numberOfSides;
+            Modifier = modifier;
+        }
+
         public int ThrowDie()
         {
             Random rnd = new();
-            return rnd.Next(1, 7);
+            int result = 0;
+            
+            for (int i = 1; i <= NoOfDice; i++)
+            {
+                result += rnd.Next(1, NoOfSides);
+            }
+
+            return result += Modifier;
+        }
+
+        public override string ToString()
+        {
+            return $"{NoOfDice}d{NoOfSides}+{Modifier}";
         }
     }
 }
