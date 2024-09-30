1a93954841955811751c1fd5022afbeff44276a5
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-09-30 14:28:04 +0200

    Refactor.

--- DungeonCrawler/GameLogic/Game.cs
+++ DungeonCrawler/GameLogic/Game.cs
@@ -1,4 +1,5 @@
 ﻿using DungeonCrawler.Elements;
+using System.Threading.Channels;
 
 namespace DungeonCrawler.GameLogic
 {
@@ -6,59 +7,58 @@ namespace DungeonCrawler.GameLogic
     {
         Player player;
 
-        
         public void SetupGame()
         {
             Console.Title = "Dungeon Crawler Deluxe Edition";
             Console.CursorVisible = false;
-            
 
             string filePath = @"Levels\Level1.txt";
             LevelData.Load(filePath);
             player = (Player)LevelData.MapElements.Find(findPlayer => findPlayer.MapSymbol == '@');
-            
+            DrawGame();
         }
+
         public void PlayGame()
         {
             while (true)
             {
-                // TODO: Flytta till egen metod?.
-                // Rita upp kartan igen.
-                foreach (var item in LevelData.MapElements)
-                {
-                    if (item is Wall wall)
-                    {
-                        if (wall.IsVisible == true)
-                            Console.ForegroundColor = wall.VisibleColour;
-                        else if (wall.HasBeenDetected)
-                            Console.ForegroundColor = wall.HasBeenDetectedColour;
-                        else
-                            Console.ForegroundColor = wall.InVisibleColour;
-
-                        DistanceController.CheckDistance(player, wall);
-                        Console.SetCursorPosition(wall.XPosition, wall.YPosition);
-                        wall.Draw();
-                    }
-                    else if (item is Rat rat)
-                    {
-                        DistanceController.CheckDistance(player, rat);
-                        rat.Movement();
-                        rat.Draw();
-                    }
-                    else if (item is Snake snake)
-                    {
-                        DistanceController.CheckDistance(player, snake);
-                        //snake.Movement();
-                        snake.Draw();
-                    }
-                }
-
                 player.PlayerMovement();
                 player.Draw();
-                Thread.Sleep(50);
+                DrawGame();
+                Thread.Sleep(150);
             }
         }
 
+        public void DrawGame()
+        {
+            foreach (var item in LevelData.MapElements)
+            {
+                if (item is Wall wall)
+                {
+                    if (wall.IsVisible == true)
+                        Console.ForegroundColor = wall.VisibleColour;
+                    else if (wall.HasBeenDetected)
+                        Console.ForegroundColor = wall.HasBeenDetectedColour;
+                    else
+                        Console.ForegroundColor = wall.InVisibleColour;
 
+                    DistanceController.CheckDistance(player, wall);
+                    Console.SetCursorPosition(wall.XPosition, wall.YPosition);
+                    wall.Draw();
+                }
+                else if (item is Rat rat)
+                {
+                    DistanceController.CheckDistance(player, rat);
+                    rat.Update();
+                    //rat.Draw();
+                }
+                else if (item is Snake snake)
+                {
+                    DistanceController.CheckDistance(player, snake);
+                    //snake.Movement();
+                    snake.Draw();
+                }
+            }
+        }
     }
 }
