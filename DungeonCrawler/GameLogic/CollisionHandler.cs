af00f56835d0687a4e30d52c0661903d46e8e33c
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-04 07:31:28 +0200

    Add OnAttacking and Rafactor by adding a local function.

--- DungeonCrawler/GameLogic/CollisionHandler.cs
+++ DungeonCrawler/GameLogic/CollisionHandler.cs
@@ -5,99 +5,69 @@ namespace DungeonCrawler.GameLogic
     internal static class CollisionHandler
     {
         public static LevelElement collisionObject;
+        
+        public delegate void AttackEventHandler(LevelElement source, LevelElement receiver);
+        public static event AttackEventHandler Attacking;
 
-        public static bool CheckForCollision(Enum directionMoved, LevelElement character)
+        public static bool CheckForCollision(Enum directionMoved, LevelElement elementThatMoved)
         {
             if (directionMoved.Equals(Directions.North))
             {
-                var item = LevelData.MapElements.Find(item => item.XPosition == character.XPosition && item.YPosition == character.YPosition - 1);
-
-                if (item != null)
-                {
-                    collisionObject = item;
-                    return true;
-                }
-                else
-                    return false;
+                var item = LevelData.MapElements.Find(item => item.XPosition == elementThatMoved.XPosition 
+                    && item.YPosition == elementThatMoved.YPosition - 1);
+                return HasCollided(elementThatMoved, item);
             }
             else if (directionMoved.Equals(Directions.South))
             {
-                var item = LevelData.MapElements.Find(item => item.XPosition == character.XPosition && item.YPosition == character.YPosition + 1);
-
-                if (item != null)
-                {
-                    collisionObject = item;
-                    return true;
-                }
-                else
-                    return false;
-
+                var item = LevelData.MapElements.Find(item => item.XPosition == elementThatMoved.XPosition 
+                    && item.YPosition == elementThatMoved.YPosition + 1);
+                return HasCollided(elementThatMoved, item);
             }
             else if (directionMoved.Equals(Directions.West))
             {
-                var item = LevelData.MapElements.Find(item => item.YPosition == character.YPosition && item.XPosition == character.XPosition - 1);
-
-                if (item != null)
-                {
-                    collisionObject = item;
-                    return true;
-                }
-                else
-                    return false;
-
+                var item = LevelData.MapElements.Find(item => item.YPosition == elementThatMoved.YPosition 
+                    && item.XPosition == elementThatMoved.XPosition - 1);
+                return HasCollided(elementThatMoved, item);
             }
             else
             {
-                var item = LevelData.MapElements.Find(item => item.YPosition == character.YPosition && item.XPosition == character.XPosition + 1);
+                var item = LevelData.MapElements.Find(item => item.YPosition == elementThatMoved.YPosition
+                    && item.XPosition == elementThatMoved.XPosition + 1);
+                return HasCollided(elementThatMoved, item);
+            }
 
+
+            static bool HasCollided(LevelElement elementThatMoved, LevelElement? item)
+            {
                 if (item != null)
                 {
                     collisionObject = item;
+                    if (elementThatMoved is Player)
+                        OnAttacking(elementThatMoved as Player);
+                    else if (elementThatMoved is Enemy)
+                        OnAttacking(elementThatMoved as Enemy);
+
                     return true;
                 }
                 else
                     return false;
             }
-        }
-        public static void PerformAttack(LevelElement attacker, LevelElement defender)
-        {
-            int result = 0;
-
-            Enemy enemy = null;
-            Player player = null;
-
-            if (attacker is Player && defender is Enemy)
-            {
-                player = attacker as Player;
-                enemy = defender as Enemy;
-                result = player.AttackDice.ThrowDie() - enemy.DefenceDice.ThrowDie();
-                if (result < 0)
-                    result = 0;
-
-                enemy.Health -= result;
-                
-                TextHandler.PlayerAttacksText(player, enemy, result);
-
-            }
-            else if(attacker is Enemy && defender is Player)
-            {
-                enemy = attacker as Enemy;
-                player = defender as Player;
-                result = enemy.AttackDice.ThrowDie() - player.DefenceDice.ThrowDie();
-                if (result < 0)
-                    result = 0;
-
-                player.Health -= result;
-                TextHandler.EnemyAttacksText(player, enemy, result);
-            }
-
-            
         }
 
         public static void ClearOldPosition(LevelElement element)
         {
             Console.SetCursorPosition(element.XPosition, element.YPosition);
             Console.Write(" ");
+        }
+
+        public static void OnAttacking(LevelElement characterThatAttacked)
+        {
+            if (characterThatAttacked is Player)
+                Attacking?.Invoke(characterThatAttacked as Player, collisionObject as Enemy);
+            else if (characterThatAttacked is Enemy)
+                Attacking?.Invoke(characterThatAttacked as Enemy, collisionObject as Player);
+            else
+                return;
         }
         
     }
