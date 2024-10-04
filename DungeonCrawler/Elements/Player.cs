54bbaea2a167ff3a6f5290e793c446b970db5845
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-04 15:40:30 +0200

    Remove an if statement

--- DungeonCrawler/Elements/Player.cs
+++ DungeonCrawler/Elements/Player.cs
@@ -6,14 +6,13 @@ namespace DungeonCrawler.Elements
     {
         public string Name { get; set; }
         public int Health { get; set; }
-        public int NumberOfStepsTaken { get; set; } = 0;
         public bool HasCollided { get; set; } = false;
         public Dice AttackDice { get; set; }
         public Dice DefenceDice { get; set; }
 
         ConsoleKeyInfo keyInfo;
         ConsoleKey keyPressed;
-        
+
         public Player()
         {
             // PLayer: HP = 100, Attack = , Defence =
@@ -32,98 +31,76 @@ namespace DungeonCrawler.Elements
         public void PlayerMovement()
         {
             bool isKeyPressed = false;
-            
-            while(!isKeyPressed)
-            {
-                if (Console.KeyAvailable)
-                {
-                    keyInfo = Console.ReadKey(true);
-                    keyPressed = keyInfo.Key;
 
-                    switch (keyPressed)
-                    {
-                        case ConsoleKey.W:
-                            if (CollisionController.CheckForCollision(Directions.North, this))
-                            {
-                                isKeyPressed = true;
-                                break;
-                            }
-                            else
-                            {
-                                CollisionController.ClearOldPosition(this);
-                                this.YPosition--;
-                                isKeyPressed = true;
-                                break;
-                            }
-                        case ConsoleKey.S:
-                            if (CollisionController.CheckForCollision(Directions.South, this))
-                            {
-                                isKeyPressed = true;
-                                break;
-                            }
-                            else
-                            {
-                                CollisionController.ClearOldPosition(this);
-                                this.YPosition++;
-                                isKeyPressed = true;
-                                break;
-                            }
-                        case ConsoleKey.A:
-                            if (CollisionController.CheckForCollision(Directions.West, this))
-                            {
-                                isKeyPressed = true;
-                                break;
-                            }
-                            else
-                            {
-                                CollisionController.ClearOldPosition(this);
-                                this.XPosition--;
-                                isKeyPressed = true;
-                                break;
-                            }
-                        case ConsoleKey.D:
-                            if (CollisionController.CheckForCollision(Directions.East, this))
-                            {
-                                isKeyPressed = true;
-                                break;
-                            }
-                            else
-                            {
-                                CollisionController.ClearOldPosition(this);
-                                this.XPosition++;
-                                isKeyPressed = true;
-                                break;
-                            }
-                        case ConsoleKey.Spacebar:
+            while (!isKeyPressed)
+            {
+
+                keyInfo = Console.ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                switch (keyPressed)
+                {
+                    case ConsoleKey.W:
+                        if (CollisionController.CheckForCollision(Directions.North, this))
+                        {
                             isKeyPressed = true;
                             break;
-                        case ConsoleKey.Escape:
+                        }
+                        else
+                        {
+                            CollisionController.ClearOldPosition(this);
+                            this.YPosition--;
                             isKeyPressed = true;
-                            Environment.Exit(0);
                             break;
-                    }
+                        }
+                    case ConsoleKey.S:
+                        if (CollisionController.CheckForCollision(Directions.South, this))
+                        {
+                            isKeyPressed = true;
+                            break;
+                        }
+                        else
+                        {
+                            CollisionController.ClearOldPosition(this);
+                            this.YPosition++;
+                            isKeyPressed = true;
+                            break;
+                        }
+                    case ConsoleKey.A:
+                        if (CollisionController.CheckForCollision(Directions.West, this))
+                        {
+                            isKeyPressed = true;
+                            break;
+                        }
+                        else
+                        {
+                            CollisionController.ClearOldPosition(this);
+                            this.XPosition--;
+                            isKeyPressed = true;
+                            break;
+                        }
+                    case ConsoleKey.D:
+                        if (CollisionController.CheckForCollision(Directions.East, this))
+                        {
+                            isKeyPressed = true;
+                            break;
+                        }
+                        else
+                        {
+                            CollisionController.ClearOldPosition(this);
+                            this.XPosition++;
+                            isKeyPressed = true;
+                            break;
+                        }
+                    case ConsoleKey.Spacebar:
+                        isKeyPressed = true;
+                        break;
+                    case ConsoleKey.Escape:
+                        isKeyPressed = true;
+                        Environment.Exit(0);
+                        break;
                 }
             }
-            NumberOfStepsTaken++;
-
-            if (NumberOfStepsTaken > 5 && HasCollided)
-            {
-                TextHandler.ClearEventText();
-                NumberOfStepsTaken = 0;
-                HasCollided = false;
-            }
         }
-
-        public void OnAttacking(LevelElement source, LevelElement receiver)
-        {
-            Player attacker = source as Player;
-            Enemy defender = receiver as Enemy;
-
-            if (attacker is Player && defender is Enemy)
-                TextHandler.PlayerAttacksText(attacker, defender, 0);
-            else
-                return;
-        }
-
     }
 }
