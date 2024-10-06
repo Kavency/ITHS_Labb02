9583993e41432934148cdba2475ba0a238ddc137
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-06 12:21:10 +0200

    Delete OnAttacking.

--- DungeonCrawler/Elements/Enemy.cs
+++ DungeonCrawler/Elements/Enemy.cs
@@ -9,26 +9,6 @@ namespace DungeonCrawler.Elements
         public Dice AttackDice { get; set; }
         public Dice DefenceDice { get; set; }
 
-        internal void OnAttacking(LevelElement source, LevelElement receiver)
-        {
-            int result = 0;
-            Enemy attacker = source as Enemy;
-            Player defender = receiver as Player;
-
-            if (attacker is Enemy && defender is Player)
-            {
-                result = attacker.AttackDice.ThrowDie() - defender.DefenceDice.ThrowDie();
-                if (result < 0)
-                    result = 0;
-
-                defender.Health -= result;
-
-                TextHandler.EnemyAttacksText(defender, attacker, result);
-                // Add counter attack...
-            }
-            else
-                return;
-        }
         abstract public void Update();
     }
     
