146f0bb1a6f4b08909a092ddb62350399a37619a
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-07 12:57:53 +0200

    Add ICharacter.

---
+++ DungeonCrawler/Elements/ICharacter.cs
@@ -0,0 +1,13 @@
+﻿using DungeonCrawler.GameLogic;
+
+namespace DungeonCrawler.Elements
+{
+    internal interface ICharacter
+    {
+        public int Health { get; set; }
+        public Dice AttackDice { get; set; }
+        public Dice DefenceDice { get; set; }
+        public void Died();
+
+    }
+}
