6c77916de59919a04dd01ce18e9f9032714b84af
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-07 16:41:42 +0200

    Add props and method.

--- DungeonCrawler/Elements/ICharacter.cs
+++ DungeonCrawler/Elements/ICharacter.cs
@@ -5,9 +5,12 @@ namespace DungeonCrawler.Elements
     internal interface ICharacter
     {
         public int Health { get; set; }
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
         public Dice AttackDice { get; set; }
         public Dice DefenceDice { get; set; }
         public void Died();
+        public void Move();
 
     }
 }
