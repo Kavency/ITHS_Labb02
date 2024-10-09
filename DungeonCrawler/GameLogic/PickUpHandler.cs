36bacf7eb39460e6e2ead7b27b602904c70c00a5
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-09 11:25:37 +0200

    Add class

---
+++ DungeonCrawler/GameLogic/PickUpHandler.cs
@@ -0,0 +1,24 @@
+﻿using DungeonCrawler.Elements;
+using DungeonCrawler.Elements.Items;
+
+namespace DungeonCrawler.GameLogic
+{
+    internal static class PickUpHandler
+    {
+        public static void PickUpItem(Player player, LevelElement item)
+        {
+            if (item is Key key)
+            {
+                key.PickUp(player);
+            }
+            else if (item is Torch torch)
+            {
+                torch.PickUp(player);
+            }
+            else if (item is HealthPotion potion)
+            {
+                potion.PickUp(player);
+            }
+        }
+    }
+}
