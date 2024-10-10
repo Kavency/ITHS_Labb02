9c0cb952a6d22ae476295dd4b7f401eb0873cc2f
Magnus "Kavency" Hellman <53920093+Kavency@users.noreply.github.com>
2024-10-10 20:22:22 +0200

    Add class

---
+++ DungeonCrawler/GameLogic/SoundController.cs
@@ -0,0 +1,14 @@
+﻿using System.Media;
+
+namespace DungeonCrawler.GameLogic
+{
+    internal static class SoundController
+    {
+        public static SoundPlayer musicPlayer = new(@".\Assets\Music\BGMusic.wav");
+                
+        public static void PlayMusic()
+        {
+            musicPlayer.PlayLooping();
+        }
+    }
+}
